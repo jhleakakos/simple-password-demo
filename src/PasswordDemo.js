9fc276261c209c34b2e45da289f9dd6dad5a5544
Joseph Leakakos <49359469+jhleakakos@users.noreply.github.com>
2022-02-11 21:49:36 -0700

    add count for number of iterations to crack password with brute force; add code to handle six characters in password for when commenting out portions of array for characters to check against

--- src/PasswordDemo.js
+++ src/PasswordDemo.js
@@ -10,34 +10,81 @@ import {
 const PasswordDemo = () => {
     const checkCharacters = [
         ...lowercaseLetters,
-        ...uppercaseLetters,
-        ...numbers,
-        ...specialCharacters
+        // ...uppercaseLetters,
+        // ...numbers,
+        // ...specialCharacters
     ];
 
     const [password, setPassword] = useState('');
     const [cracked, setCracked] = useState(false);
+    const [count, setCount] = useState(0);
 
     useEffect(() => setCracked(false), [password]);
 
     const crackPassword = () => {
-        const pwdLength = password.length;
+        let checkCount = 0;
 
-        for (let firstLetter of checkCharacters) {
-            let passwordGuess = firstLetter;
-            if (passwordGuess === password) return setCracked(true);
+        for (let letter1 of checkCharacters) {
+            checkCount += 1;
+            let passwordGuess1 = letter1;
+            if (passwordGuess1 === password) {
+                setCount(checkCount);
+                setCracked(true);
+                return
+            }
 
-            for (let secondLetter of checkCharacters) {
-                passwordGuess = `${firstLetter}${secondLetter}`;
-                if (passwordGuess === password) return setCracked(true);
+            for (let letter2 of checkCharacters) {
+                checkCount += 1;
+                let passwordGuess2 = `${passwordGuess1}${letter2}`;
+                if (passwordGuess2 === password) {
+                    setCount(checkCount);
+                    setCracked(true);
+                    return
+                }
 
-                for (let thirdLetter of checkCharacters) {
-                    passwordGuess = `${firstLetter}${secondLetter}${thirdLetter}`;
-                    if (passwordGuess === password) return setCracked(true);
+                for (let letter3 of checkCharacters) {
+                    checkCount += 1;
+                    let passwordGuess3 = `${passwordGuess2}${letter3}`;
+                    if (passwordGuess3 === password) {
+                        setCount(checkCount);
+                        setCracked(true);
+                        return
+                    }
+
+                    for (let letter4 of checkCharacters) {
+                        checkCount += 1;
+                        let passwordGuess4 = `${passwordGuess3}${letter4}`;
+                        if (passwordGuess4 === password) {
+                            setCount(checkCount);
+                            setCracked(true);
+                            return
+                        }
+
+                        for (let letter5 of checkCharacters) {
+                            checkCount += 1;
+                            let passwordGuess5 = `${passwordGuess4}${letter5}`;
+                            if (passwordGuess5 === password) {
+                                setCount(checkCount);
+                                setCracked(true);
+                                return
+                            }
+
+                            for (let letter6 of checkCharacters) {
+                                checkCount += 1;
+                                let passwordGuess6 = `${passwordGuess5}${letter6}`;
+                                if (passwordGuess6 === password) {
+                                    setCount(checkCount);
+                                    setCracked(true);
+                                    return
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
     }
+
     return (
         <div>
             <h1>Simple Password Demo</h1>
@@ -46,12 +93,14 @@ const PasswordDemo = () => {
             <label htmlFor='pwd'>Password: </label>
             <input
                 id='pwd'
-                type='password'
+                type='text'
+                maxLength='6'
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
             />
             <button onClick={crackPassword}>Crack Me!</button>
             <p>{cracked ? "Your password's been cracked!" : 'Good password...within reason'}</p>
+            {cracked ? <p>Number of checks to get to your password: {count}</p> : ''}
         </div>
     );
 }
